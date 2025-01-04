3001816dcd87ab7a0710a206e52445c76c5976af
Yasir Akhtar <yasircpr@gmail.com>
2025-01-05 03:34:02 +0530

    New Music !

--- BABYMUSIC/utils/stream/stream.py
+++ BABYMUSIC/utils/stream/stream.py
@@ -12,6 +12,7 @@ from BABYMUSIC.utils.database import add_active_video_chat, is_active_chat
 from BABYMUSIC.utils.exceptions import AssistantErr
 from BABYMUSIC.utils.inline import aq_markup, close_markup, stream_markup
 from BABYMUSIC.utils.stream.queue import put_queue, put_queue_index
+from BABYMUSIC.utils.pastebin import PROBin
 
 
 async def stream(
@@ -112,7 +113,7 @@ async def stream(
         if count == 0:
             return
         else:
-            link = await BABYBin(msg)
+            link = await PROBin(msg)
             lines = msg.count("\n")
             if lines >= 17:
                 car = os.linesep.join(msg.split(os.linesep)[:17])
